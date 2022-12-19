955e7242f7fbb1c411ac54419e3b76211484d484
pranit67 <pranit67@gmail.com>
2022-12-19 12:50:29 +0530

    Adding Program to Calculate Laps

---
+++ Bridgelabz238/src/Homework/LapsCalculator.java
@@ -0,0 +1,10 @@
+package Homework;
+
+public class LapsCalculator {
+    public static void main(String[] args) {
+        int lapsPerDay = 12;
+        int days = 8;
+        int overall = lapsPerDay * days;
+        System.out.println("Ajay runs "+ overall + " laps in "+days+ " day.");
+    }
+}
